b178d2402db97cc07c53061d30fa4ef5d3d7b457
Inhar <aldalurinhar33@gmail.com>
2025-04-08 17:40:34 +0100

    fix linter deploy

--- landing/src/app/layout.tsx
+++ landing/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
-import { ExtensionAuthBridge } from '@/components/ExtensionAuthBridge';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
+import ExtensionAuthBridge from '../components/ExtensionAuthBridge';
 
 const poppins = Poppins({
 	subsets: ['latin'],
